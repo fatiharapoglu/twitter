40864ab4c6ede2184b53464c1c79039b05b59a04
Fatih Arapoglu <f3tt4n@gmail.com>
2023-04-06 22:43:19 +0300

    add formik, yup

--- src/components/root/SignUpDialog.tsx
+++ src/components/root/SignUpDialog.tsx
@@ -2,8 +2,9 @@ import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 type DialogProps = {
     open: boolean;
@@ -11,28 +12,102 @@ type DialogProps = {
 };
 
 export default function SignUpDialog({ open, handleSignUpClose }: DialogProps) {
+    const validationSchema = yup.object({
+        email: yup.string().email("Enter a valid email.").required("Email is required."),
+        password: yup
+            .string()
+            .min(8, "Password should be of minimum 8 characters length.")
+            .max(100, "Password should be of maximum 100 characters length.")
+            .required("Password is required."),
+        username: yup
+            .string()
+            .min(3, "Username should be of minimum 3 characters length.")
+            .max(30, "Username should be of maximum 30 characters length.")
+            .required("Username is required."),
+        name: yup.string().max(50, "Name should be of maximum 50 characters length."),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            username: "",
+            name: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values));
+        },
+    });
+
     return (
-        <Dialog open={open} onClose={handleSignUpClose}>
-            <DialogTitle>Subscribe</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We will send
-                    updates occasionally.
-                </DialogContentText>
-                <TextField
-                    autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Email Address"
-                    type="email"
-                    fullWidth
-                    variant="standard"
-                />
-            </DialogContent>
-            <DialogActions>
-                <button onClick={handleSignUpClose}>Cancel</button>
-                <button onClick={handleSignUpClose}>Subscribe</button>
-            </DialogActions>
+        <Dialog className="dialog" open={open} onClose={handleSignUpClose}>
+            <DialogTitle className="title">Create your account</DialogTitle>
+            <form onSubmit={formik.handleSubmit}>
+                <DialogContent>
+                    <div className="input-group">
+                        <div className="input">
+                            <div className="info">Your login information</div>
+                            <TextField
+                                fullWidth
+                                required
+                                id="email"
+                                name="email"
+                                label="Email"
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                error={formik.touched.email && Boolean(formik.errors.email)}
+                                helperText={formik.touched.email && formik.errors.email}
+                            />
+                        </div>
+                        <div className="input">
+                            <TextField
+                                fullWidth
+                                required
+                                id="password"
+                                name="password"
+                                label="Password"
+                                type="password"
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                error={formik.touched.password && Boolean(formik.errors.password)}
+                                helperText={formik.touched.password && formik.errors.password}
+                            />
+                        </div>
+                        <div className="input">
+                            <div className="info">Your public name (@username)</div>
+                            <TextField
+                                fullWidth
+                                required
+                                id="username"
+                                name="username"
+                                label="Username"
+                                value={formik.values.username}
+                                onChange={formik.handleChange}
+                                error={formik.touched.username && Boolean(formik.errors.username)}
+                                helperText={formik.touched.username && formik.errors.username}
+                            />
+                        </div>
+                        <div className="input">
+                            <div className="info">What do you want to be seen like?</div>
+                            <TextField
+                                fullWidth
+                                id="name"
+                                name="name"
+                                label="Name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                error={formik.touched.name && Boolean(formik.errors.name)}
+                                helperText={formik.touched.name && formik.errors.name}
+                            />
+                        </div>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <button onClick={handleSignUpClose}>Cancel</button>
+                    <button type="submit">Create</button>
+                </DialogActions>
+            </form>
         </Dialog>
     );
 }
