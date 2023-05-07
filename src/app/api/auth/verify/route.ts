5df915ef155f25fe48f7f3b4ca52efec3c6aa3cd
Fatih Arapoglu <f3tt4n@gmail.com>
2023-05-07 16:42:34 +0300

    move jwt verifying logic to backend

---
+++ src/app/api/auth/verify/route.ts
@@ -0,0 +1,15 @@
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+import { getJwtSecretKey } from "@/utilities/auth";
+
+export async function POST(request: NextRequest) {
+    const token = await request.json();
+
+    try {
+        const { payload } = await jwtVerify(token, getJwtSecretKey());
+        return NextResponse.json(payload);
+    } catch (error) {
+        return NextResponse.json(null);
+    }
+}
--- src/utilities/auth/index.ts
+++ src/utilities/auth/index.ts
@@ -1,16 +1,22 @@
-import { jwtVerify } from "jose";
+const verifyTokenFromServer = async (token: string) => {
+    const response = await fetch("http://localhost:3000/api/auth/verify", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(token),
+    });
+    return response.json();
+};
 
 export const getJwtSecretKey = () => {
-    const key = process.env.NEXT_PUBLIC_JWT_SECRET_KEY;
+    const key = process.env.JWT_SECRET_KEY;
     if (!key) throw new Error("No JWT secret key");
     return new TextEncoder().encode(key);
 };
 
 export const verifyJwtToken = async (token: string) => {
-    try {
-        const { payload } = await jwtVerify(token, getJwtSecretKey());
-        return payload;
-    } catch (error) {
-        return null;
-    }
+    const response = await verifyTokenFromServer(token);
+    if (!response) return null;
+    return response;
 };
