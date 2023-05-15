26d74150b6d14b920998353da81a2339d70be56d
Fatih Arapoglu <f3tt4n@gmail.com>
2023-05-15 18:22:33 +0300

    testing api if error caused by this

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -6,6 +6,8 @@ import { getJwtSecretKey } from "@/utilities/auth";
 export async function POST(request: NextRequest) {
     const token = await request.json();
 
+    console.log(getJwtSecretKey());
+
     try {
         const { payload } = await jwtVerify(token, getJwtSecretKey());
         return NextResponse.json(payload);
